1ed8995f1d28fe0200318177fa9aa966dcaf3b3a
Traben <ben.trabant@gmail.com>
2022-09-14 18:03:47 +1000

    4.1.0

--- common/src/main/java/traben/entity_texture_features/mixin/MixinPackScreen.java
+++ common/src/main/java/traben/entity_texture_features/mixin/MixinPackScreen.java
@@ -2,6 +2,7 @@ package traben.entity_texture_features.mixin;
 
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.pack.PackScreen;
+import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.gui.widget.TexturedButtonWidget;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
@@ -9,6 +10,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import traben.entity_texture_features.ETFVersionDifferenceHandler;
 import traben.entity_texture_features.config.screens.ETFConfigScreenMain;
 
 import java.util.Objects;
@@ -26,7 +28,7 @@ public abstract class MixinPackScreen extends Screen {
 
     @Inject(method = "init", at = @At("TAIL"))
     private void etf$illegalPathOverride(CallbackInfo ci) {
-        if (!ETFConfigData.hideConfigButton) {
+        if (!ETFConfigData.hideConfigButton && (ETFVersionDifferenceHandler.isFabric() == ETFVersionDifferenceHandler.isThisModLoaded("fabric"))) {
             this.addButton(new TexturedButtonWidget((this.width - 48), (this.height - 48), 24, 20,
                     0, 0, 20, new Identifier(MOD_ID + ":textures/gui/settings.png"), 24, 40,
                     (button) -> Objects.requireNonNull(client).openScreen(new ETFConfigScreenMain(this))));
