74cec29a30ecdc4415e5c0d1265f05463493f181
Traben <ben.trabant@gmail.com>
2024-01-02 10:37:20 +1000

    [5.1.1] - fixed 3d skin layers mod & etf skin feature, compat for the 1.6 update *(also removed log warning spam for future changes to 3d skin layers)*. - restructured the config - generified the emissive and enchanted pixel rendering methods - the mini entities in mob spawners UUID's should now always use the following format in nbt *("[I;?,?,12345,12345]")* making them identifiable for randomisation, the blocks property should also return the actual mob spawner block.

--- common/src/main/java/traben/entity_texture_features/mixin/entity/block/MixinMobSpawnerLogic.java
+++ common/src/main/java/traben/entity_texture_features/mixin/entity/block/MixinMobSpawnerLogic.java
@@ -5,6 +5,7 @@ import net.minecraft.entity.Entity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -15,14 +16,24 @@ import java.util.UUID;
 public abstract class MixinMobSpawnerLogic {
 
 
+    @Unique
+    private static final long ETF$SPAWNER_MARKER = (12345L << 32) + 12345L;
+
     @Inject(method = "getRenderedEntity",
             at = @At(value = "RETURN"))
     private void etf$stabiliseMobSpawnerUUID(World world, BlockPos pos, CallbackInfoReturnable<Entity> cir) {
         Entity entity = cir.getReturnValue();
         if(entity != null){
-            entity.setUuid(new UUID(pos.asLong(), world.hashCode()));
+            entity.setUuid(new UUID(pos.asLong(), ETF$SPAWNER_MARKER));
+            entity.setPos(pos.getX(),pos.getY(),pos.getZ());
+            //resulting nbt should be [I;?,?,12345,12345]
         }
     }
+
+//copy of how nbt splits up uuid values
+//    private static int[] toIntArray(long uuidMost, long uuidLeast) {
+//        return new int[]{(int)(uuidMost >> 32), (int)uuidMost, (int)(uuidLeast >> 32), (int)uuidLeast};
+//    }
 }
 
 
