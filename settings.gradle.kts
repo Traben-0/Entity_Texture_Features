40580f905ea081ae137165c50b6004493a15f26a
Traben <ben.trabant@gmail.com>
2025-12-27 23:25:04 +1000

    github build testing

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,9 +15,9 @@ pluginManagement {
     // We also recommend specifying your desired version here if you're using more than one of the plugins,
     // so you do not have to change the version in multilpe places when updating.
     plugins {
-        val egtVersion = "0.6.11-test2"
-        id("gg.essential.multi-version.root") version egtVersion
-        id("gg.essential.multi-version.api-validation") version egtVersion
+        //val egtVersion = "0.6.11-test2"
+        id("gg.essential.multi-version.root") //version egtVersion
+        id("gg.essential.multi-version.api-validation") //version egtVersion
     }
 }
 
