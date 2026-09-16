41fa0306f319e0654d5a0c1a6fe888d06d48f9d0
Traben <ben.trabant@gmail.com>
2026-09-16 10:02:15 +1000

    changelog

--- src/main/java/traben/entity_texture_features/features/state/ETFState.java
+++ src/main/java/traben/entity_texture_features/features/state/ETFState.java
@@ -113,14 +113,14 @@ public abstract class ETFState {
     }
 
     public static void stackVerify(ETFEntityRenderState shouldBeHere) {
-        boolean log = ETF.config().getConfig().stackDebugPrinting;
         if (shouldBeHere == null) {
-            if (log) ETFUtils2.logWarn("ETFState stack verify called with null, this is likely a bug");
+            if (ETF.config().getConfig().stackDebugPrinting) ETFUtils2.logWarn("ETFState stack verify called with null, this is likely a bug");
             stackVerifyHasFailed = true;
             return;
         }
 
         if (state() != shouldBeHere) {
+            boolean log = ETF.config().getConfig().stackDebugPrinting;
             stackVerifyHasFailed = false;
             if (log) ETFUtils2.logWarn("ETFState stack is not correct");
             if (stateStack.contains(shouldBeHere)) {
